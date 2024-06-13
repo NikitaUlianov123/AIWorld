6ff54e363d4754082f73a48298ced4013ae35dd8
Nikita.Ulianov <Nikita.Ulianov@GMRML2>
2024-06-12 21:24:12 -0700

    made graph and frontier

--- AIWorld/Graph.cs
+++ AIWorld/Graph.cs
@@ -1,8 +1,9 @@
-﻿\using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AIWorld
 {
@@ -11,10 +12,55 @@ namespace AIWorld
         List<Vertex<T>> vertices;
 
         public Graph()
-        { 
+        {
             vertices = new List<Vertex<T>>();
         }
 
+        public void AddVertex(T value)
+        {
+            vertices.Add(new Vertex<T>(value));
+        }
 
+        public void AddEdge(T start, T end, int weight)
+        {
+            vertices.First(x => x.Value.Equals(start)).Neighbors.Add((vertices.First(y => y.Value.Equals(end)), weight));
+        }
+
+
+        public List<Vertex<T>> Search(T start, T end, IFrontier<T> frontier)
+        {
+            List<Vertex<T>> visited = new List<Vertex<T>>();
+            Dictionary<Vertex<T>, Vertex<T>> founders = new Dictionary<Vertex<T>, Vertex<T>>();//Node, founder
+
+            var first = vertices.First(x => x.Value.Equals(start));
+            frontier.Add(first);
+            visited.Add(first);
+
+
+            while (!frontier.Contains(end))
+            {
+                GoNext();
+            }
+
+            List<Vertex<T>> result = new List<Vertex<T>>();
+            var current = vertices.First(y => y.Value.Equals(end));//fix later
+            while (current != null)
+            {
+                result.Insert(0, current);
+                current = founders[current];
+            }
+
+            return result;
+
+            void GoNext()
+            {
+                foreach (var neighbor in frontier.Next.Neighbors)
+                {
+                    frontier.Add(neighbor.destination);
+                    founders.Add(frontier.Next, neighbor.destination);
+                }
+                frontier.RemoveNext();
+            }
+        }
     }
 }
---
+++ AIWorld/IFrontier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AIWorld
+{
+    public interface IFrontier<T>
+    {
+        Vertex<T> Next { get;}
+
+        void Add(Vertex<T> vertex);
+
+        void RemoveNext();
+
+        bool Contains(Vertex<T> vertex);
+        bool Contains(T value) => frontier.Where(x => x.Value.Equals(value)).Count() > 0;
+    }
+
+    public class BFS<T> : IFrontier<T>
+    {
+        public ICollection<Vertex<T>> frontier { get => Collections.sort(myList, pq.comparator()); }
+        public Vertex<T> Next { get => priorityQueue.Peek(); }
+
+        PriorityQueue<Vertex<T>, int> priorityQueue;
+
+        public BFS()
+        { 
+            
+        }
+
+        public void Add(Vertex<T> vertex)
+        {
+            priorityQueue.Enqueue(vertex);
+        }
+
+        public void RemoveNext()
+        {
+            priorityQueue.Dequeue();
+        }
+
+        bool Contains(Vertex<T> vertex) => frontier.Contains(vertex);
+    }
+}
