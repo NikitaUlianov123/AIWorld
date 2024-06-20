33b3825ba9c492ada38d0d7b99bbd61f219c57aa
Nikita Ulianov <person@mectruck.com>
2024-06-19 21:19:17 -0700

    Started ExpectiMax

---
+++ AIWorld/IGameState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWorld
+{
+    public interface IGameState
+    {
+        bool IsTerminal { get; }
+
+        float Score { get; set; }
+    }
+
+    public struct Successor<T> where T : IGameState
+    {
+        public T State;
+        public int Cost;
+        public float Chance;
+
+        public Successor(T state, int cost, float chance)
+        {
+            State = state;
+            Cost = cost;
+            Chance = chance;
+        }
+    }
+}
