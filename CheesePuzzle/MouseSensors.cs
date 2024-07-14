49e4cbabdf383bd49ab72b97ef6637f08bb24506
Nikita Ulianov <person@mectruck.com>
2024-07-13 20:19:03 -0700

    Fixed all syntax errors, still need to fix logic errors after transition to sensors.

---
+++ CheesePuzzle/MouseSensors.cs
@@ -0,0 +1,26 @@
+﻿using AIWorld;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheesePuzzle
+{
+    public class MouseSensors : ISensorReading
+    {
+        public byte[] values { get; set; }
+
+        public bool IsTerminal { get; private set; }
+
+        public float Score { get; private set; }
+
+        public MouseSensors(CheeseState state)
+        {
+            IsTerminal = state.IsTerminal;
+            Score = state.Score;
+            values = [(byte)state.Mouse.X, (byte)state.Mouse.Y];
+        }
+    }
+}
